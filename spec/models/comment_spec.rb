9026ea5eae321c26c5f2e096ef0137c702e42d59
Theofwz <theo@futureworkz.com>
2016-07-11 13:58:39 +0700

    add comment_spec#comment_parent

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -9,8 +9,16 @@ RSpec.describe Comment, type: :model do
   describe 'Associations' do
     it { is_expected.to have_many :replies }
     it { is_expected.to have_many :likes }
-    
+
     it { is_expected.to belong_to :user }
     it { is_expected.to belong_to :parent }
+  end
+
+  describe '#comment_parent' do
+    let!(:comment_parent)  { create(:comment, parent_id: nil) }
+
+    it 'get comment_parent' do
+      expect(Comment.comment_parent).to include comment_parent
+    end
   end
 end
